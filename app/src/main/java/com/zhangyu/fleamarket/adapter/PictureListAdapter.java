d87f7b88fd1c258453d99b4dd5d0cffc943f8d28
xgao <xgao@wandoujia.com>
2015-05-27 17:19:12 +0800

    Implementation ImageLoader

--- app/src/main/java/com/zhangyu/fleamarket/adapter/PictureListAdapter.java
+++ app/src/main/java/com/zhangyu/fleamarket/adapter/PictureListAdapter.java
@@ -8,7 +8,9 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.etsy.android.grid.StaggeredGridView;
 import com.zhangyu.fleamarket.R;
+import com.zhangyu.fleamarket.Utils.ImageDownloader;
 
 import java.util.HashMap;
 import java.util.List;
@@ -17,6 +19,7 @@ import java.util.List;
  * Created by gaoxiong on 2015/5/26.
  */
 public class PictureListAdapter extends BaseAdapter {
+
   final class ViewHolder {
     ImageView image;
     TextView title;
@@ -25,10 +28,13 @@ public class PictureListAdapter extends BaseAdapter {
 
   private Context mContext;
   private List<HashMap<String, Object>> dataList;
+  private ImageDownloader imageDownloader;
 
-  public PictureListAdapter(Context context, List<HashMap<String, Object>> dataList) {
+  public PictureListAdapter(Context context, List<HashMap<String, Object>> dataList, StaggeredGridView gridView) {
     this.mContext = context;
     this.dataList = dataList;
+    imageDownloader = new ImageDownloader();
+    imageDownloader.setMode(ImageDownloader.Mode.CORRECT);
   }
 
   @Override
@@ -48,19 +54,20 @@ public class PictureListAdapter extends BaseAdapter {
 
   @Override
   public View getView(int i, View view, ViewGroup viewGroup) {
-    ViewHolder holder;
+    final ViewHolder holder;
     if (view == null) {
       holder = new ViewHolder();
       view = LayoutInflater.from(mContext).inflate(R.layout.picture_list_item, null);
       holder.image = (ImageView) view.findViewById(R.id.ItemImage);
       holder.title = (TextView) view.findViewById(R.id.ItemTitle);
       holder.text = (TextView) view.findViewById(R.id.ItemText);
+      final String imageUrl = (String) getItem(i).get("ItemImageUrl");
+      imageDownloader.download(imageUrl, holder.image);
       view.setTag(holder);
     } else {
       holder = (ViewHolder) view.getTag();
     }
 
-    holder.image.setImageResource((Integer) getItem(i).get("ItemImage"));
     holder.title.setText((String) getItem(i).get("ItemTitle"));
     holder.text.setText((String) getItem(i).get("ItemText"));
 
