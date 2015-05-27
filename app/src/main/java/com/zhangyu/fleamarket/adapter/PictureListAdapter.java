70d475e8abb9cfa3b822c5f166b6c5167f26a781
xgao <xgao@wandoujia.com>
2015-05-27 20:04:02 +0800

    Use volley

--- app/src/main/java/com/zhangyu/fleamarket/adapter/PictureListAdapter.java
+++ app/src/main/java/com/zhangyu/fleamarket/adapter/PictureListAdapter.java
@@ -1,6 +1,8 @@
 package com.zhangyu.fleamarket.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,9 +10,11 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
 import com.etsy.android.grid.StaggeredGridView;
 import com.zhangyu.fleamarket.R;
-import com.zhangyu.fleamarket.utils.ImageDownloader;
 
 import java.util.HashMap;
 import java.util.List;
@@ -28,13 +32,16 @@ public class PictureListAdapter extends BaseAdapter {
 
   private Context mContext;
   private List<HashMap<String, Object>> dataList;
-  private ImageDownloader imageDownloader;
+//  private ImageDownloader imageDownloader;
+  private ImageLoader imageLoader;
 
   public PictureListAdapter(Context context, List<HashMap<String, Object>> dataList, StaggeredGridView gridView) {
     this.mContext = context;
     this.dataList = dataList;
-    imageDownloader = new ImageDownloader();
-    imageDownloader.setMode(ImageDownloader.Mode.CORRECT);
+    RequestQueue mQueue = Volley.newRequestQueue(context);
+    imageLoader = new ImageLoader(mQueue, new BitmapCache());
+//    imageDownloader = new ImageDownloader();
+//    imageDownloader.setMode(ImageDownloader.Mode.CORRECT);
   }
 
   @Override
@@ -54,15 +61,16 @@ public class PictureListAdapter extends BaseAdapter {
 
   @Override
   public View getView(int i, View view, ViewGroup viewGroup) {
-    final ViewHolder holder;
+    ViewHolder holder = null;
+    final String imageUrl = (String) getItem(i).get("ItemImageUrl");
     if (view == null) {
       holder = new ViewHolder();
       view = LayoutInflater.from(mContext).inflate(R.layout.picture_list_item, null);
       holder.image = (ImageView) view.findViewById(R.id.ItemImage);
       holder.title = (TextView) view.findViewById(R.id.ItemTitle);
       holder.text = (TextView) view.findViewById(R.id.ItemText);
-      final String imageUrl = (String) getItem(i).get("ItemImageUrl");
-      imageDownloader.download(imageUrl, holder.image);
+      //imageDownloader.download(imageUrl, holder.image);
+
       view.setTag(holder);
     } else {
       holder = (ViewHolder) view.getTag();
@@ -71,9 +79,34 @@ public class PictureListAdapter extends BaseAdapter {
     holder.title.setText((String) getItem(i).get("ItemTitle"));
     holder.text.setText((String) getItem(i).get("ItemText"));
 
-//    int ColorPos = i % colors.length;
-//    view.setBackgroundColor(colors[ColorPos]);
+    ImageLoader.ImageListener imageListener = ImageLoader.getImageListener(holder.image,
+      android.R.drawable.ic_menu_rotate, android.R.drawable.ic_delete);
+    imageLoader.get(imageUrl, imageListener);
 
     return view;
+  }
+
+  private class BitmapCache implements ImageLoader.ImageCache {
+    private LruCache<String, Bitmap> bitmapLruCache;
+
+    public BitmapCache() {
+      int maxSize = 10 * 1024 * 1024;
+      bitmapLruCache = new LruCache<String, Bitmap>(maxSize) {
+        @Override
+        protected int sizeOf(String key, Bitmap value) {
+          return value.getRowBytes() * value.getHeight();
+        }
+      };
+    }
+
+    @Override
+    public Bitmap getBitmap(String url) {
+      return bitmapLruCache.get(url);
+    }
+
+    @Override
+    public void putBitmap(String url, Bitmap bitmap) {
+      bitmapLruCache.put(url, bitmap);
+    }
   }
 }
